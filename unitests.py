8dffa3918872c3fa0bce1d1e803cafb477c15fa4
Guy Heller <hellerguyh@gmail.com>
2022-06-29 15:51:34 +0300

    add scatter plot to unitests.py'
    Signed-off-by: Guy Heller <hellerguyh@gmail.com>

--- unitests.py
+++ unitests.py
@@ -39,7 +39,7 @@ def showHistogram(path, label):
     plt.savefig("histogram.png")
 
 def smallTest():
-    loader = getDL(1, True, "MNIST", True)
+    loader = getDL(1, True, "MNIST", True, False)
     for img, label in loader:
         ml_sample = img
         break
@@ -60,31 +60,74 @@ def smallTest():
         model.nn.eval()
         print(model.nn(ml_sample))
 
+def showScatterPlot(path, epoch):
+    from mpl_toolkits.mplot3d import axes3d
+    import matplotlib.pyplot as plt
+    from matplotlib import style
+    style.use('ggplot')
+    ds = MetaDS(path, True)
+    xx = 1 
+    yy = 9
+    zz = 0
+    for j in [0]:
+        for i in range(epoch):
+            X, X_T, Y, Y_T = ds.getMalPred(i, [xx, yy, zz])
+            fig = plt.figure()
+            X = np.concatenate([X, X_T])
+            Y = np.concatenate([Y, Y_T])
+            #ax1 = fig.add_subplot(111, projection='3d')
+            tagged_predictions = X[Y == 1]
+            untagged_predictions = X[Y == 0]
+            plt.scatter(tagged_predictions.T[0 + j], tagged_predictions.T[1 + j], c='g', marker='o', label="tagged")
+            plt.scatter(untagged_predictions.T[0 + j], untagged_predictions.T[1 + j], c='b', marker='o', label="untagged")
+            plt.xlabel(str(xx))
+            plt.ylabel(str(yy))
+            #ax1.scatter(tagged_predictions.T[0], tagged_predictions.T[1], tagged_predictions.T[2], c='g', marker='o', label="tagged")
+            #ax1.scatter(untagged_predictions.T[0], untagged_predictions.T[1], untagged_predictions.T[2], c='b', marker='o', label="untagged")
+            #ax1.set_xlabel(str(xx))
+            #ax1.set_ylabel(str(yy))
+            #ax1.set_zlabel(str(zz))
+            plt.legend()
+            if j == 0:
+                plt.savefig("example_learning/tagged/" + str(i) + ".png")
+            else:
+                plt.savefig("example_learning/untagged/" + str(i) + ".png")
+            plt.close(fig)
+            #plt.show()
+
 def showHistogramFromPredictions(path, epoch):
     ds = MetaDS(path)
-    X, _, Y, _ = ds.getMalPred(epoch, list(range(10)))
+    X, _, Y, _ = ds.getMalPred(epoch, [1, 9])
     Y = np.array(Y)
-    mask = np.ma.masked_where(Y == 1, X)
-    tagged_predictions = np.ma.compressed(mask)
-    mask = np.ma.masked_where(Y == 0, X)
-    untagged_predictions = np.ma.compressed(mask)
-    print(untagged_predictions)
-    print(tagged_predictions)
-    minimum = math.floor(min([min(tagged_predictions), min(untagged_predictions)]))
-    maximum = max([max(tagged_predictions), max(untagged_predictions)])
-    bins = np.linspace(minimum, maximum, 100)  # fixed number of bins
-    plt.xlim([minimum, maximum])
-    plt.hist([tagged_predictions, untagged_predictions], label=['tagged','untagged'], bins=bins)
-    plt.legend()
-    plt.show()
+    #mask = np.ma.masked_where(Y == 1, X)
+    #tagged_predictions = np.ma.compressed(mask)
+    tagged_predictions = X[Y == 1]
+    tagged_predictions = tagged_predictions
+    #tagged_predictions = tagged_predictions.reshape(tagged_predictions.shape[0])
+    #mask = np.ma.masked_where(Y == 0, X)
+    #untagged_predictions = np.ma.compressed(mask)
+    untagged_predictions = X[Y == 0]
+    untagged_predictions = untagged_predictions
+    #untagged_predictions = untagged_predictions.reshape(untagged_predictions.shape[0])
+    plt.scatter(tagged_predictions, untagged_predictions)
+    #print(untagged_predictions)
+    #print(tagged_predictions)
+    #minimum = math.floor(min([min(tagged_predictions), min(untagged_predictions)]))
+    #maximum = max([max(tagged_predictions), max(untagged_predictions)])
+    #bins = np.linspace(minimum, maximum, 100)  # fixed number of bins
+    #plt.xlim([minimum, maximum])
+    #plt.hist([tagged_predictions, untagged_predictions], label=['tagged','untagged'], bins=bins)
+    #plt.legend()
+    #plt.show()
 
 
 if __name__ == "__main__":
-    showHistogram("ResNet18_Dictionary.pkl", [1])
+    #showHistogram("ResNet18_Dictionary.pkl", [1])
     import argparse
     parser = argparse.ArgumentParser(description="Small Tests")
     parser.add_argument("--path", type=str, default=None)
     parser.add_argument("--epoch", type=int, default=-1)
     args = parser.parse_args()
 
-    showHistogramFromPredictions(args.path, args.epoch)
+    showScatterPlot(args.path, args.epoch)
+    #showHistogramFromPredictions(args.path, args.epoch)
